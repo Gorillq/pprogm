53310d876ea0afbe12fd04fe29b06a5443bfa0c0
Gorillq <kamil.kownacki3@gmail.com>
2023-09-30 13:07:54 +0200

    initial commit

---
+++ zadanie1/zadanie.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string student = "Kamil Kownacki";
+    string number = "s30272";
+    string uczelnia = "PJATK";
+    cout << student << "\n" << "s30272" << "\n" << uczelnia;
+    return 0;
+}
