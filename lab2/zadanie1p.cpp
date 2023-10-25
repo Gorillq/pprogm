2556284b7df380568b3daaa8dff407a390767691
Gorillq <kamil.kownacki3@gmail.com>
2023-10-25 19:43:56 +0200

    pierwszy commit

---
+++ lab2/zadanie1p.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a, i, j;
+    int sum = 0;
+
+    cout << "Podaj wyraz zbioru " << endl;
+    cin >> a;
+    //petla zbioru
+    for (i = 1; i <= a; i++) {
+        int zbiory = 0;
+        for (j = 1; j <= i; j++) {
+            zbiory = zbiory + j;
+        }
+        sum = sum + zbiory;
+        if (i == a) {
+            cout << zbiory << " " << "=";
+        }
+        else {
+            cout << zbiory << " " << "+" << " ";
+        }
+    }
+    // suma zbioru
+    cout << " " << sum;
+    return 0;
+}
