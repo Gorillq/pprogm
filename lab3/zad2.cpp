26602e1c76214962c414149269df7560cac5e387
Gorillq <kamil.kownacki3@gmail.com>
2023-11-17 14:12:20 +0100

    nowy commit

--- lab3/zad2.cpp
+++ lab3/zad2.cpp
@@ -1,18 +1,32 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 using namespace std;
 
+void print(const vector<string>& lol) {
+	copy(lol.begin(), lol.end(), ostream_iterator<string>(cout, ""));
+}
 
 int main() {
-	char a;
-	for (;;) {
-		cin >> a;
-		if (a != 't') {
-			cout << a;
-			continue;
+	string wejscie;
+	char zdanie;
+	vector<string> lol;
+	cout << "Program przyjmuje input od uzytkownika i potwierdzamy enter, program bedzie zbieral input dopoki\n jakikolwiek jego element != t , wtedy program zakonczy dzialanie."; 
+	cout << endl;
+	cout << "Mozesz wpisac pierwszy prompt." << endl;
+	while (true) {
+		cin >> wejscie;
+		char* macierz = &wejscie[0];
+		auto mamyto = wejscie.find('t');
+		if (mamyto < 18446744073709551615) {
+			break;
 		}
 		else {
-			break;
-			return 0;
+			lol.push_back(wejscie);
+			print(lol);
 		}
 	}
+	return 0;
 }
+
---
+++ lab3/zad4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+class Paskal {
+public:
+	int wartosc;
+	void paskal(int x) {
+		for (int i = 0; i <= x; i++) {
+			for (int j = 0; j < x - i; j++) {
+				cout << "  ";
+			}
+			wartosc = 1;
+			for (int j = 1; j <= i; j++) {
+				cout << wartosc << "   ";
+				wartosc = wartosc * (i - j) / j;
+			}
+			cout << endl;
+		}
+	}
+};
+int main() {
+	int g;
+	Paskal obiekt;
+	cout << "Liczba wierszy: " << endl;
+	cin >> g;
+	obiekt.paskal(g);
+	return 0;
+}
