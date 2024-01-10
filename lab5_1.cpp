403d1505b28bb66fbdb79e3813c065e5cc38df86
Admin <kamil.kownacki3@gmail.com>
2024-01-10 17:32:29 +0100

    :tada:

---
+++ lab5_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+
+int main() {
+	std::cout << "Program poda najwiekszy element wektora oraz index" << std::endl;
+	std::cout << "Wartosci zatwierdzamy enter, liczby z przecinkiem odzielamy znakiem kropki np 5.5" << std::endl;
+	std::cout << std::endl;
+	std::vector<double> tablica;
+	int wielkosc;
+	double wartosc;
+	std::cout << "podaj liczbe elementow tablicy" << std::endl;
+	std::cin >> wielkosc;
+	tablica.resize(wielkosc);
+	for (int i = 0; i < wielkosc; i++) {
+		int liczba = i + 1;
+		std::cout << "Podaj wartosc " << liczba << " elementu" << std::endl;
+		std::cin >> wartosc;
+		tablica[i] = wartosc;
+	}
+	std::cout << "NAJWIEKSZY ELEMENT: " << *max_element(tablica.begin(), tablica.end()) << std::endl;
+	for (int i = 0; i < tablica.size(); i++) {
+		while (tablica[i] == *max_element(tablica.begin(), tablica.end())) {
+			std::cout << "JEGO INDEX W MATEMATYCE(?) TO " << i + 1 << std::endl;
+			std::cout << "JEGO INDEX W C++ TO " << i;
+			break;
+		}
+		continue;
+	}
+	return 0;
+}
---
+++ lab5_2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+int main() {
+	std::vector<int> wektor;
+	std::vector<int> sorter;
+	std::vector<int> zapas;
+	int wielkosc;
+	std::cout << "Podaj wielkosc tablicy liczb calkowitych" << std::endl;
+	std::cin >> wielkosc;
+	wektor.resize(wielkosc);
+	sorter.resize(wielkosc);
+	for (int i = 0; i < wektor.size(); i++) {
+		std::cout << "Wpisz element numer " << i + 1 << " i zatwierdz enter:" << std::endl;
+		std::cin >> wektor[i];
+	}
+	while (wektor.size() % 2 > 0) {
+		std::cout << "nie moge podzielic rowno wektora";
+		return 1;
+	}
+	std::copy(wektor.begin(), wektor.end(), std::back_inserter(zapas));
+	std::cout << "tablica wejsciowa:" << std::endl;
+	for (int i = 0; i < wektor.size(); i++) {
+		std::cout << wektor[i] << "\t";
+		if ((i - 1) % 2 == 0 && i != 0) {
+			std::cout << std::endl;
+			continue;
+		}
+		else {
+			continue;
+		}
+	}
+	return 0;
+}
